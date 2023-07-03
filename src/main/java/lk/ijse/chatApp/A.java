fc45bc8582955e54ca1689f5572813d5f0606d89
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-07-03 22:31:06 +0530

    client and server structure complete with login

--- src/main/java/lk/ijse/chatApp/A.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.chatApp;
-
-public class A {
-}
---
+++ src/main/java/lk/ijse/chatApp/controller/ClientChatFormController.java
@@ -0,0 +1,15 @@
+package lk.ijse.chatApp.controller;
+
+import lk.ijse.chatApp.client.Client;
+
+public class ClientChatFormController {
+    private Client client;
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
